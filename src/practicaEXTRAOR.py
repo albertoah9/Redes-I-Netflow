c194b3502c14d509ba3655ce3813acdcbb6ed518
Alberto <145713813+albertoah9@users.noreply.github.com>
2026-06-08 10:43:01 +0200

    Update practicaEXTRAOR.py, package count and ethernet header read

--- src/practicaEXTRAOR.py
+++ src/practicaEXTRAOR.py
@@ -1,4 +1,3 @@
-
 import argparse
 from rc1_pcap import pcap_open_offline, pcap_loop, pcap_close
 
@@ -21,7 +20,17 @@ def parse_args():
     return parser.parse_args()
 
 def procesa_paquete(user, pkt_header, pkt_data):
-    print("Paquete recibido")
+    user["paquetes"] += 1
+
+    if len(pkt_data) < 14:
+        return
+    
+    ethertype = int.from_bytes(pkt_data[12:14], byteorder="big")
+
+    if ethertype == 0x0800:
+        user["ipv4"] += 1
+    else:
+        user["no_ipv4"] += 1
 
 def main():
     args = parse_args()
@@ -37,7 +46,13 @@ def main():
         print(errbuf)
         return
     
-    ret = pcap_loop(pcap, -1, procesa_paquete, None)
+    estado = {
+        "paquetes": 0,
+        "ipv4": 0,
+        "no_ipv4": 0
+    }
+    
+    ret = pcap_loop(pcap, -1, procesa_paquete, estado)
 
     if ret == -1:
         print("Error durante pcap_loop")
@@ -46,8 +61,11 @@ def main():
     else:
         print("Lectura finalizada con exito")
     
+    print(f"Paquetes leidos: {estado["paquetes"]}")
+    print(f"Paquetes IPv4 {estado["ipv4"]}")
+    print(f"Paquetes no IPv4: {estado["no_ipv4"]}")
+    
     pcap_close(pcap)
 
 if __name__ == "__main__":
     main()
-
